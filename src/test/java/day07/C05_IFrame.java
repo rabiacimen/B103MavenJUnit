5210f6d1fa7bb047349496b8a152935c134f086e
mytechproed1 <mytechproed@gmail.com>
2022-12-29 14:49:17 -0600

    day07

---
+++ src/test/java/day07/C05_IFrame.java
@@ -0,0 +1,12 @@
+package day07;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class C05_IFrame extends TestBase {
+
+    @Test
+    public void iframeTest(){
+        driver.get("https://testcenter.techproeducation.com/index.php?page=iframe");
+    }
+}
